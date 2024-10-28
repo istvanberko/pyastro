13c49feb184fab7ad1a71c583e8e997d7c5f70c1
Istvan Berko <istvan.berko@gmail.com>
2024-10-27 20:32:31 -0500

    Draw player done - suprised it all worked this time!

--- main.py
+++ main.py
@@ -3,22 +3,30 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main ():
     pygame.init()
-    print ("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
     screen = pygame.display.set_mode(size = (SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH / 4
+    y = SCREEN_HEIGHT / 2
+    player = Player(x,y)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill(color=(0,0,0))
-        pygame.display.update()
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
